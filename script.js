d6404ac969ae2a91218027cc11754d2fe484b987
jirkaroz <jiri.roznovjak@gmail.com>
2014-12-07 15:00:32 -0500

    Some html functionality added. Experimented with particle bundles, but too computationally intensive.

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function sketchProc(processing) {
 /*2D interactive comet simulator. Author Jiri Roznovjak*/
-
-var PRINT = '';
 var WIDTH = 1200;
 var HEIGHT = 600;
-var G = 2000; //gravitational constant
-var DRAGGING = false;
-var pressPoint;
 processing.size(WIDTH,HEIGHT);
 document.getElementById('canvas1').style.width = WIDTH;
 document.getElementById('canvas1').style.height = HEIGHT;
+var PRINT = '';
+var G = 1000; //gravitational constant
+var DRAGGING = false;
+var PRESSPOINT;
+var STARS,COMETS;
+
+document.getElementById('debugField').innerHTML = '';
 
 var Particle = function () {
 	this.position = new processing.PVector(0,0);
@@ -19,6 +21,7 @@ var Particle = function () {
 	this.radius = 1;
 	this.color = processing.color(255,255,255);
 	this.opacity = 255;
+	this.alive = true;
 };
 
 Particle.prototype.run = function () {
@@ -58,22 +61,37 @@ Particle.prototype.getDistanceToObject = function (object) {
 	return dir.mag();
 };
 
-var Star = function(x,y,velx,vely) {
+var Star = function(x,y,velx,vely,mass) {
     Particle.call(this);
 	this.position = new processing.PVector(x,y);
 	this.velocity = new processing.PVector(velx,vely);
 	this.color = processing.color(255,255,50);
-	this.radius = 20;
 	this.opacity = 50;
+	this.mass = mass;
 };
 
 Star.prototype = Object.create(Particle.prototype);
 
 Star.prototype.display = function () {
+	this.radius = 15 + 3*this.mass;
 	processing.noStroke();
 	for (var rad=0;rad<this.radius;rad++) {
 		processing.fill(this.color,this.opacity);
 		processing.ellipse(this.position.x,this.position.y,rad*2,rad*2);
+	}
+};
+
+Star.prototype.merge = function (star2) {
+	if (this.mass < star2.mass) {
+		return
+	}
+	var distance = this.getDistanceToObject(star2);
+	if (distance < this.radius + star2.radius) {
+		this.mass += star2.mass;
+		star2.alive = false;
+		var adVel = star2.velocity;
+		adVel.mult(star2.mass/this.mass);
+		this.velocity.add(adVel);
 	}
 };
 
@@ -84,7 +102,6 @@ var Comet = function(x,y,velx,vely) {
 	this.radius = 4;
 	this.mass = 100;
 	this.tail = new Tail();
-	this.alive = true;
 };
 
 Comet.prototype = Object.create(Particle.prototype);
@@ -163,7 +180,7 @@ var Tail = function () {
 };
 
 Tail.prototype.addDust = function (x,y,opacity) {
-	for (var i=0;i<10;i++) {
+	for (var i=0;i<12/STARS.length;i++) {
 		this.dustArray.push(new DustParticle(x,y,opacity));
 	}
 };
@@ -183,7 +200,7 @@ var DustParticle = function(x,y,opacity) {
 	this.velocity = new processing.PVector((Math.random()-0.5)*0.6,(Math.random()-0.5)*0.6);
 	this.color = processing.color(220,220,220);
 	this.opacity = opacity;
-	this.radius = 2;
+	this.radius = 3;
 	this.drift = 0.95;
 };
 
@@ -192,15 +209,14 @@ DustParticle.prototype = Object.create(Particle.prototype);
 DustParticle.prototype.run = function () {
 	this.update();
 	this.opacity -= 3;
-	processing.noStroke();
-	processing.fill(this.color,this.opacity);
-	processing.ellipse(this.position.x,this.position.y,this.radius*2,this.radius*2);
+	this.display();
 };
 
 DustParticle.prototype.update = function () {
 	Particle.prototype.update.call(this);
 	this.velocity.mult(this.drift);
 };
+
 
 DustParticle.prototype.checkInvisible = function () {
 	if (this.opacity < 0) {
@@ -210,42 +226,71 @@ DustParticle.prototype.checkInvisible = function () {
 
 var createComet = function () {
 	var currentPoint = new processing.PVector(processing.mouseX,processing.mouseY);
-	var velocity = PVector.sub(pressPoint,currentPoint);
+	var velocity = PVector.sub(PRESSPOINT,currentPoint);
 	velocity.div(40);
-	comets.push(new Comet(processing.mouseX,processing.mouseY,velocity.x,velocity.y));
+	COMETS.push(new Comet(processing.mouseX,processing.mouseY,velocity.x,velocity.y));
 };
 
-var sideVelocity = 2;
-var stars = [new Star(WIDTH/2,HEIGHT/2+150,sideVelocity,0),new Star(WIDTH/2,HEIGHT/2-150,-sideVelocity,0)];
-var comets = [];
+var createStar = function () {
+	var currentPoint = new processing.PVector(processing.mouseX,processing.mouseY);
+	var velocity = PVector.sub(PRESSPOINT,currentPoint);
+	velocity.div(40);
+	STARS.push(new Star(processing.mouseX,processing.mouseY,velocity.x,velocity.y,1));
+};
+
+var setupAnimation = function () {
+	COMETS = [];
+	var sideVelocitySmall = Math.random()*0.5 + 1;
+	var sideVelocityBig = 6;
+	//one massive and two dwarf stars
+	//STARS = [new Star(WIDTH/2,HEIGHT/2,0,0,8),new Star(WIDTH/2-40,HEIGHT/2-250,sideVelocityBig,-sideVelocitySmall,0.15),new Star(WIDTH/2+30,HEIGHT/2-250,sideVelocityBig,sideVelocitySmall,0.15)];
+	//binary with two same stars
+	STARS = [new Star(WIDTH/2,HEIGHT/2-200,sideVelocitySmall,0,2),new Star(WIDTH/2,HEIGHT/2+200,-sideVelocitySmall,0,2)];
+	//one star
+	//STARS = [new Star(WIDTH/2,HEIGHT/2,0,0,2)];
+	//STARS = [new Star(WIDTH/2-50,HEIGHT/2-250,0,-sideVelocitySmall,0.1),new Star(WIDTH/2+50,HEIGHT/2-250,0,sideVelocitySmall,0.3)];
+};
 
 processing.draw = function () {
+	if (RESET) {
+		setupAnimation();
+		RESET = false;
+	}
+	//document.getElementById('debugField').innerHTML = STARS.length;
 	processing.background(18,24,64);
-	for (var i=0; i<comets.length;i++) {
-		for (var j=0; j<stars.length; j++) {
-			comets[i].runCometAndTail(stars[j]);
+	for (var i=0; i<COMETS.length;i++) {
+		for (var j=0; j<STARS.length; j++) {
+			COMETS[i].runCometAndTail(STARS[j]);
 		}
 	}
-	for (var i=0; i<stars.length; i++) {
-		for (var j=0; j<stars.length; j++) {
+	for (var i=0; i<STARS.length; i++) {
+		for (var j=0; j<STARS.length; j++) {
 			if (j!=i) {
-				var gravity = stars[i].calculateAttraction(stars[j]);
-				stars[i].applyForce(gravity);
+				var gravity = STARS[i].calculateAttraction(STARS[j]);
+				STARS[i].applyForce(gravity);
+				STARS[i].merge(STARS[j]);
 			}
 		}
-		stars[i].run();
+		STARS[i].run();
+		if (!STARS[i].alive) {
+			STARS.splice(i,1);
+			if (i != STARS.length) {
+				i--;
+			}
+		}
 	}
 };
 
 processing.mousePressed = function () {
-	pressPoint = new processing.PVector(processing.mouseX,processing.mouseY);
+	PRESSPOINT = new processing.PVector(processing.mouseX,processing.mouseY);
 };
 
 processing.mouseReleased = function () {
+	//createStar();
 	createComet();
 };
 
-document.getElementById('debugField').innerHTML = PRINT;
+setupAnimation();
 }
 var canvas = document.getElementById("canvas1");
 var p = new Processing(canvas, sketchProc);
