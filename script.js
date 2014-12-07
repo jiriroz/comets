2e5a487a69bc98786b2895373e12e28dd7400480
jirkaroz <jiri.roznovjak@gmail.com>
2014-12-06 20:24:48 -0500

    Tail added

--- script.js
+++ script.js
@@ -28,9 +28,18 @@ var Comet = function (x,y,velx,vely) {
 	this.position = new processing.PVector(x,y);
 	this.velocity = new processing.PVector(velx,vely);
 	this.acceleration = new processing.PVector(0,0);
-	this.radius = 3;
-	this.mass = 1;
+	this.radius = 4;
+	this.mass = 100;
 	this.tail = new Tail();
+	this.color = processing.color(255,255,255);
+	this.opacity = 255;
+};
+
+Comet.prototype.runCometAndTail = function (star) {
+	if (this.isAlive()) {
+		this.run(star);
+	}
+	this.runTail(star);
 };
 
 Comet.prototype.run = function (star) {
@@ -38,6 +47,16 @@ Comet.prototype.run = function (star) {
 	this.applyForce(gravity);
 	this.update();
 	this.display();
+};
+
+Comet.prototype.runTail = function (star) {
+	var opacity = this.determineDustOpacity(star);
+	if (this.isAlive()) {
+		this.tail.addDust(this.position.x,this.position.y,opacity);
+		}
+	this.tail.update();
+	this.decreaseMass(star);
+	//this.opacity = 255 * (this.mass/100);
 };
 
 Comet.prototype.calculateAttraction = function (object) {
@@ -55,46 +74,82 @@ Comet.prototype.applyForce = function (force) { //force as a pvector
 	this.acceleration.add(force);
 };
 
-Comet.prototype.update = function () {
+Comet.prototype.update = function (star) {
 	this.velocity.add(this.acceleration);
 	this.position.add(this.velocity);
 	this.acceleration.mult(0);
-	processing.fill(255,255,255);
-	this.tail.addDust(this.position.x,this.position.y);
+	this.radius = 1 + 2*(this.mass/100);
+	document.getElementById('debugField').innerHTML = this.radius;
+};
+
+Comet.prototype.determineDustOpacity = function (star) {
+	var distance = this.getDistanceToStar(star);
+	var opacity = 255 * (100 / distance);
+	if (opacity > 255) {opacity = 255}
+	return opacity;
+};
+
+Comet.prototype.getDistanceToStar = function (star) {
+	var dir = PVector.sub(star.position,this.position);
+	return dir.mag();
+};
+
+ //decreases comet's mass based on its proximity to the star
+Comet.prototype.decreaseMass = function (star) {
+	var distance = this.getDistanceToStar(star);
+	this.mass -= 3000 / (distance*distance);
+	if (this.mass == 0) {this.mass = -1;}
 };
 
 Comet.prototype.display = function () {
 	processing.noStroke();
-	processing.fill(255,255,255);
+	processing.fill(this.color,this.opacity);
 	processing.ellipse(this.position.x,this.position.y,this.radius*2,this.radius*2);
+};
+
+Comet.prototype.isAlive = function () {
+	if (this.mass < 0) {
+		return false;
+	}
+	return true;
 };
 
 var Tail = function () {
-	this.tailArray = [];
+	this.dustArray = [];
+	this.color = processing.color(220,220,220);
 };
 
-Tail.prototype.addDust = function (x,y) {
-	this.tailArray.push(new Dust(x,y));
+Tail.prototype.addDust = function (x,y,opacity) {
+	this.dustArray.push(new DustParticle(x,y,opacity));
 };
 
 Tail.prototype.update = function () {
-	for (var i=0; i<this.tailArray.length;i++) {
-		this.tailArray[i].update();
+	for (var i=0; i<this.dustArray.length;i++) {
+		this.dustArray[i].run();
+		if (this.dustArray[i].checkInvisible()) {
+			this.dustArray.splice(i,1);
+		}
 	}
 };
 
-var Dust = function (x,y) {
+var DustParticle = function (x,y,opacity) {
 	this.position = new processing.PVector(x,y);
 	this.color = processing.color(220,220,220);
-	this.opacity = 100;
+	this.opacity = opacity;
 	this.radius = 2;
 };
 
-Dust.prototype.update = function () {
-	//this.opacity = 0;
+DustParticle.prototype.run = function () {
+	this.opacity -= 3;
 	processing.noStroke();
 	processing.fill(this.color,this.opacity);
 	processing.ellipse(this.position.x,this.position.y,this.radius*2,this.radius*2);
+};
+
+DustParticle.prototype.checkInvisible = function () {
+	if (this.opacity < 0) {
+		return true;
+	}
 };
 
 var createComet = function () {
@@ -110,7 +165,7 @@ var comets = [];
 processing.draw = function () {
 	processing.background(18,24,64);
 	for (var i=0; i<comets.length;i++) {
-		comets[i].run(star);
+		comets[i].runCometAndTail(star);
 	}
 	star.display();
 };
